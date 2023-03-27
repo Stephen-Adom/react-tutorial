fd850674fd56635102d76a31e31da255125e8fec
Stephen Adom Addae <saaddae003@st.ug.edu.gh>
2023-03-27 09:00:07 +0000

    set up input for adding todo

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 const InputTodo = () => {
-	return <div>InputTodo</div>;
+	const [title, setTitle] = useState("");
+
+	const handleChange = (e) => {
+		setTitle(e.target.value);
+	};
+
+	return (
+		<form>
+			<input
+				type="text"
+				placeholder="Add Todo..."
+				value={title}
+				onChange={handleChange}
+			/>
+			<button>Submit</button>
+		</form>
+	);
 };
 
 export default InputTodo;
